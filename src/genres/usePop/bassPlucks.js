a38acd3125895dd5ac41564aaa949b963b7eca35
markcalendario <markcalendario@gmail.com>
2024-07-11 09:26:06 +0800

    feat: bass

--- src/genres/usePop/bassPlucks.js
+++ src/genres/usePop/bassPlucks.js
@@ -1,14 +1,4 @@
 export default {
-  A: [
-    { notes: ["A0"], duration: 1 },
-    { notes: [""], duration: 1 },
-    { notes: [""], duration: 1 },
-    { notes: ["A0"], duration: 1 },
-    { notes: ["A0"], duration: 1 },
-    { notes: [""], duration: 1 },
-    { notes: [""], duration: 1 },
-    { notes: [""], duration: 1 }
-  ],
   C: [
     { notes: ["C0"], duration: 1 },
     { notes: [""], duration: 1 },
@@ -19,32 +9,22 @@ export default {
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 }
   ],
-  E: [
-    { notes: ["E0"], duration: 1 },
+  Dm: [
+    { notes: ["D0"], duration: 1 },
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 },
-    { notes: ["E0"], duration: 1 },
-    { notes: ["E0"], duration: 1 },
+    { notes: ["D0"], duration: 1 },
+    { notes: ["D0"], duration: 1 },
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 }
   ],
-  G: [
-    { notes: ["G0"], duration: 1 },
+  Em: [
+    { notes: ["E0"], duration: 1 },
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 },
-    { notes: ["G0"], duration: 1 },
-    { notes: ["G0"], duration: 1 },
-    { notes: [""], duration: 1 },
-    { notes: [""], duration: 1 },
-    { notes: [""], duration: 1 }
-  ],
-  D: [
-    { notes: ["D0"], duration: 1 },
-    { notes: [""], duration: 1 },
-    { notes: [""], duration: 1 },
-    { notes: ["D0"], duration: 1 },
-    { notes: ["D0"], duration: 1 },
+    { notes: ["E0"], duration: 1 },
+    { notes: ["E0"], duration: 1 },
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 }
@@ -59,6 +39,16 @@ export default {
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 }
   ],
+  G: [
+    { notes: ["G0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: ["G0"], duration: 1 },
+    { notes: ["G0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 }
+  ],
   Am: [
     { notes: ["A0"], duration: 1 },
     { notes: [""], duration: 1 },
@@ -69,7 +59,87 @@ export default {
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 }
   ],
-  Dm: [
+  D: [
+    { notes: ["D0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: ["D0"], duration: 1 },
+    { notes: ["D0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 }
+  ],
+  "F#m": [
+    { notes: ["F#0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: ["F#0"], duration: 1 },
+    { notes: ["F#0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 }
+  ],
+  A: [
+    { notes: ["A0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: ["A0"], duration: 1 },
+    { notes: ["A0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 }
+  ],
+  Bm: [
+    { notes: ["B0"], duration: 1 },
+    { notes: ["B0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: ["B0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 }
+  ],
+  E: [
+    { notes: ["E0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: ["E0"], duration: 1 },
+    { notes: ["E0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 }
+  ],
+  "F#": [
+    { notes: ["F#0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: ["F#0"], duration: 1 },
+    { notes: ["F#0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 }
+  ],
+  "G#m": [
+    { notes: ["G#0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: ["G#0"], duration: 1 },
+    { notes: ["G#0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 }
+  ],
+  B: [
+    { notes: ["B0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: ["B0"], duration: 1 },
+    { notes: ["B0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 }
+  ],
+  "C#m": [
     { notes: ["C#0"], duration: 1 },
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 },
@@ -79,12 +149,32 @@ export default {
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 }
   ],
-  Em: [
-    { notes: ["F0"], duration: 1 },
+  Gm: [
+    { notes: ["G0"], duration: 1 },
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 },
-    { notes: ["F0"], duration: 1 },
-    { notes: ["F0"], duration: 1 },
+    { notes: ["G0"], duration: 1 },
+    { notes: ["G0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 }
+  ],
+  Bb: [
+    { notes: ["A#0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: ["A#0"], duration: 1 },
+    { notes: ["A#0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 }
+  ],
+  "D#m": [
+    { notes: ["D#0"], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: [""], duration: 1 },
+    { notes: ["D#0"], duration: 1 },
+    { notes: ["D#0"], duration: 1 },
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 },
     { notes: [""], duration: 1 }
